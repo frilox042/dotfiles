4ae5810268889a208a82609b50992319e8a98660
Gether Damien <damiengether@gmail.com>
2020-03-30 23:34:43 +0200

    migrate to sway

---
+++ .config/sway/i3barconfig.py
@@ -0,0 +1,36 @@
+"""i3 bar config file"""
+
+from i3pystatus import Status
+
+status = Status()  # pylint: disable=I0011,C0103
+
+status.register("clock",
+                format="%a %-d %b %X",)
+
+status.register("mem",
+                format="{avail_mem}",)
+
+status.register("load",
+                format="{avg1}",)
+
+status.register("network",
+                interface="eno1",
+                format_up="{v4cidr}",
+                format_down="",)
+
+status.register("network",
+                interface="wlp2s0",
+                format_up="{v4cidr} {essid} {quality:03.0f}%",
+                format_down="",)
+
+status.register("disk",
+                path="/",
+                format="{used}/{total}G [{avail}G]",)
+
+status.register("pulseaudio",
+                format="♪{volume}",)
+
+status.register("xkblayout",
+                layouts=["fr", "us"])
+
+status.run()
